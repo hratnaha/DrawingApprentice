6345459f68d0a7839811c86bc7b9d76d917ccf6c
Chih-Pin Hsiao <chipin01@gmail.com>
2015-10-27 14:25:16 -0400

    add facebook login

---
+++ server/configuration/config.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+	"facebook_api_key" 		: "1668411693393703",
+    "facebook_api_secret"	: "d059a2cb9fda79d68964e82b96a28212",
+    "callback_url"			: "http://localhost:3000/auth/facebook/callback",
+    "use_database"			: "false",
+    "host"					: "localhost",
+    "username"				: "root",
+    "password"				: "",
+    "database"				: ""
+}
