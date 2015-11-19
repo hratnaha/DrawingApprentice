5f8399a13c6e7d440da5de50194a06f5f7518244
Chih-Pin Hsiao <chipin01@gmail.com>
2015-11-20 01:24:13 +0800

    version with Lisa's new front end page

---
+++ server/public/app/js/sketchPad.js
@@ -0,0 +1,217 @@
+//var canvas = "{}";
+
+function sketchUtil() {
+	
+    // get the canvas element and its context
+    var container = document.getElementById('container');
+    bothCanvas.setAttribute('width', container.offsetWidth);
+    bothCanvas.setAttribute('height', container.offsetHeight);
+
+    var canvas = document.getElementById('sketchpad');
+    canvas.setAttribute('width', container.offsetWidth);
+    canvas.setAttribute('height', container.offsetHeight);
+    var context = canvas.getContext('2d');
+    context.lineWidth = 1;
+	
+	
+    var curstroke;
+    var strCounter = 0, pkptCounter = 0;
+    var colorline;
+    
+    function createNewStroke() {
+        strCounter++;
+        pkptCounter = 0;
+        
+        var now = (new Date()).getTime();
+        var newstroke = {
+                       // create a temporary stroke for sending
+            type : "stroke",
+            data : {
+                timestamp : now,
+                id : "stroke_" + strCounter,
+                color : {
+                    r: 1,
+                    g: 0,
+                    b: 0,
+                    a: 1
+                },
+                packetPoints : []
+            }
+        };
+        return newstroke;
+    }
+    function pushNewPacketPoint(coors) {
+        var now = (new Date()).getTime();
+        pkptCounter++;
+        // construct a new packet point
+        var pkpt = {
+            id : "pt_" + pkptCounter,
+            x : coors.x,
+            y : coors.y,
+            timestamp : now,
+            pressure : pressurevalue,
+            color: colorline          //passing color in hex form
+        };
+        
+        curstroke.data.packetPoints.push(pkpt);
+    }
+    
+	
+    // create a drawer which tracks touch movements
+    var drawer = {
+        isDrawing: false,
+        touchstart: function (coors) {
+            curstroke = createNewStroke();
+			if ($('#grouping').hasClass("isGrouping")){
+            //if ($("#cboxGrouping").attr('checked') == "checked") {
+				
+				//alert("grouping checked");
+                context.setLineDash([5]);
+                context.strokeStyle = x;
+				console.log(x);
+				context.globalAlpha = opacity2;
+	
+            } else {
+                colorline = document.getElementById('background').value;
+                context.strokeStyle = x;
+                //context.setLineDash([0]);
+				context.lineWidth = y;
+				context.globalAlpha = opacity2;
+	
+            }
+
+            context.beginPath();
+            context.moveTo(coors.x, coors.y);
+            this.isDrawing = true;
+        },
+        touchmove: function (coors) {
+            if (this.isDrawing) {
+                context.lineTo(coors.x, coors.y);
+                context.stroke();
+				context.lineWidth = y;
+				context.globalAlpha = opacity2;
+	
+                var json_coor = JSON.stringify(coors); //converting to json
+                pushNewPacketPoint(coors);
+            }
+        },
+        touchend: function (coors) {
+            if (this.isDrawing) {
+                this.touchmove(coors);
+                var stringStroke = JSON.stringify(curstroke);
+                onTouchUp(stringStroke);
+                
+                var height = canvas.height;
+
+                this.isDrawing = false;
+				//if (document.getElementById('grouping').clicked == true) {
+                if (!$('#grouping').hasClass("isGrouping"))
+                    bothInputContext.drawImage(canvas, 0, 0);
+
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    };
+    // create a function to pass touch events and coordinates to drawer
+    function draw(event) {
+        var type = null;
+        var timer = null;
+        
+        var now = new Date();
+        var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+        
+        // map mouse events to touch events
+        switch (event.type) {
+            case "mousedown":
+                event.touches = [];
+                event.touches[0] = {
+                    pageX: event.pageX,
+                    pageY: event.pageY
+                };
+                type = "touchstart";
+                break;
+            case "mousemove":
+                event.touches = [];
+                event.touches[0] = {
+                    pageX: event.pageX,
+                    pageY: event.pageY
+                };
+                type = "touchmove";
+                
+                break;
+            case "mouseup":
+                event.touches = [];
+                event.touches[0] = {
+                    pageX: event.pageX,
+                    pageY: event.pageY
+                };
+                type = "touchend";
+                clearTimeout(timer);
+                break;
+            
+        }
+        
+        // touchend clear the touches[0], so we need to use changedTouches[0]
+        var coors;
+        if (event.type === "touchend") {
+            coors = {
+                x: event.changedTouches[0].pageX - this.parentElement.offsetLeft,
+                y: event.changedTouches[0].pageY - this.parentElement.offsetTop
+            };
+        }
+        else {
+            // get the touch coordinates
+            coors = {
+                x: event.touches[0].pageX - this.parentElement.offsetLeft,
+                y: event.touches[0].pageY - this.parentElement.offsetTop
+            };
+        }
+        type = type || event.type;
+        // pass the coordinates to the appropriate handler
+        drawer[type](coors);
+    }
+    
+    // detect touch capabilities
+    var touchAvailable = ('createTouch' in document) || ('ontouchstart' in window);
+    
+    // attach the touchstart, touchmove, touchend event listeners.
+    if (touchAvailable) {
+        canvas.addEventListener('touchstart', draw, false);
+        canvas.addEventListener('touchmove', draw, false);
+        canvas.addEventListener('touchend', draw, false);
+    }
+    // attach the mousedown, mousemove, mouseup event listeners.
+    else {
+        canvas.addEventListener('mousedown', draw, false);
+        canvas.addEventListener('mousemove', draw, false);
+        canvas.addEventListener('mouseup', draw, false);
+    }
+    
+    // prevent elastic scrolling
+    document.body.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+    }, false); // end body.onTouchMove
+
+}
+//Pressure implementation
+var pressurevalue = 0;
+var log = function (msg) {
+    $("<div>").text(msg).appendTo($("#log"));
+    console.log(msg);
+};
+var logtouch = function (evtype, t) {
+    pressurevalue = t.webkitForce;
+}
+
+var mouseIsDown = false;
+
+function pressure1() {
+    
+    $(document).on("mousemove", function () { log("mousemove"); });
+    $(document).on("touchstart touchmove touchend touchcancel", function (ev) {3
+        $.each(ev.originalEvent.touches, function (i, t) { logtouch(ev.type + "-touches", t); });
+        $.each(ev.originalEvent.changedTouches, function (i, t) { logtouch(ev.type + "-changed", t); });
+        $.each(ev.originalEvent.targetTouches, function (i, t) { logtouch(ev.type + "-target", t); });
+    });
+}
+pressure1();
---
+++ server/public/app/js/sketchio_client.js
@@ -0,0 +1,262 @@
+var ioUri = "http://localhost:8080"; //replace with the Websocket URL
+var output;
+var socket;
+var botCanvas = {};
+var isdrawing = false;
+var ison = true;
+var curStroke = [];
+var finishStroke = false;
+var lineThickness;
+
+function initWebSocket() {
+    botCanvas = document.getElementById('botpad');
+	sketchPadCanvas = document.getElementById('sketchpad');
+	moveLogo = document.getElementById("logo");
+
+    botCanvas.setAttribute('width', container.offsetWidth);
+    botCanvas.setAttribute('height', container.offsetHeight);
+
+    output = document.getElementById("output");
+    socket = io.connect(ioUri);
+
+    socket.on('newconnection', onOpen);
+    socket.on('respondStroke', onNewStroke);
+    socket.on('allData', onDataReceived);
+    socket.on('disconnected', saveDataOnDb);
+
+	var logo = document.getElementById("logo");
+
+    var i = 0;
+    var botStroke = "";
+    var ctx = botCanvas.getContext('2d');
+	var ctx2 = sketchPadCanvas.getContext('2d');
+    ctx.lineWidth = 0.1;
+
+    $('#ex8').slider().on('slideStop', function (ev) {
+        console.log('Current Creativity Value:' + ' ' + ev.value / 100);
+        socket.emit("SetCreativty", ev.value);
+    });
+
+    var timer = setInterval(function () {
+
+        if (botStroke != "" && i < botStroke.packetPoints.length ) {
+            ctx.lineTo(botStroke.packetPoints[i].x, botStroke.packetPoints[i].y);
+			//console.log(botStroke.packetPoints[i].x);
+
+            ctx.stroke();
+			ctx.strokeStyle = x;
+			ctx.globalAlpha = opacity2;
+			ctx.lineWidth = y;
+
+            //console.log(botStroke.packetPoints[i].x);
+			moveLogo.style.left = botStroke.packetPoints[i].x - 70;
+			moveLogo.style.top = botStroke.packetPoints[i].y - 130;
+			//moveLogo.style.backgroundColor = "blue";
+
+            i++;
+        } else if (curStroke.length > 0) {
+            botStroke = curStroke.shift();
+            ctx.beginPath();
+            ctx.moveTo(botStroke.packetPoints[0].x, botStroke.packetPoints[0].y);
+			ctx.strokeStyle = x;
+			ctx.globalAlpha = opacity2;
+			ctx.lineWidth = y;
+            i = 0;
+			//moveLogo.style.left = botStroke.packetPoints[i].x - 70;
+			//moveLogo.style.top = botStroke.packetPoints[i].y - 130;
+			//moveLogo.style.backgroundColor = "red";
+
+
+        } else if (botStroke != "") {
+            bothInputContext.drawImage(botCanvas, 0, 0);
+            ctx.clearRect(0, 0, botCanvas.width, botCanvas.height);
+            botStroke = "";
+            i = 0;
+			//moveLogo.style.backgroundColor = "yellow";
+			MoveLogoBack();
+        }
+    }, 20);
+
+}
+
+
+function MoveLogoBack () {
+	if(finishStroke==false){
+	//console.log("move");
+	//moveLogo.style.left = '4em';
+	//moveLogo.style.top = '5em';
+			$('#logo').animate({
+					left: '90%',
+					top: '-1em'},
+				"swing");
+
+	console.log('logo left is ' + moveLogo.style.left);
+	}
+}
+
+
+function onNewStroke(data) {
+	moveLogo.style.left = "90%";
+			moveLogo.style.top = "3%";
+    console.log(data);
+    // decode the data into the new stroke
+    var botStroke = JSON.parse(data);
+    curStroke.push(botStroke);
+	logo.style.position = "absolute";
+	logo.style.left = data.x;
+	logo.style.top = data.y;
+
+	//var botPts = botStroke.packetPoints;
+
+ //   if (botPts.length > 1) {
+
+ //       ctx.beginPath();
+ //       ctx.moveTo(botPts[0].x, botPts[0].y);
+	//	var i = 0;
+
+	//}
+}
+
+function onOpen(data) {
+    var hello = {
+        width : container.offsetWidth,
+        height: container.offsetHeight,
+        user: userData,
+        sessionId: sessionId
+    };
+    socket.emit("onOpen", hello);
+}
+
+function onDataReceived(allData) {
+    console.log('data received');
+    console.log(allData);
+
+    var timestamp = String(Date.now());
+
+    var userLines = allData.userLines;
+    var computerLines = allData.computerLines;
+
+    var userBlob = new Blob([userLines],
+        {type: "text/plain;charset=utf-8"});
+    saveAs(userBlob, timestamp + "userLines.txt");
+
+    setTimeout(function() {
+        var computerBlob = new Blob([computerLines],
+            {type: "text/plain;charset=utf-8"});
+        saveAs(computerBlob, timestamp + "computerLines.txt");
+    }, 500);
+}
+
+function onTouchUp(message) {
+    socket.emit('touchup', message);
+}
+
+function onTouchDown() {
+    socket.emit('touchdown');
+}
+
+function writeToScreen(message) {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+}
+// save a image to the client's computer
+function saveToImage() {
+
+}
+// clear the canvas
+function clearCanvas() {
+	alert('clear canvas');
+	myCanvasContext1.clearRect(0, 0, myCanvas1.width, myCanvas1.height);
+	myCanvasContext2.clearRect(0, 0, myCanvas2.width, myCanvas2.height);
+	myCanvasContext3.clearRect(0, 0, myCanvas3.width, myCanvas3.height);
+	//context.clearRect(0, 0, canvas.width, canvas.height);
+    socket.emit('clear', 'all');
+}
+// change the mode base on the UI changes
+
+function setMode(mode) {
+
+    switch ($(this).val()) {
+        case 'local':
+            m = 0;
+            break;
+        case 'regional':
+            m = 1;
+            break;
+        case 'global':
+            m = 2;
+            break;
+    }
+    socket.emit('setMode', m);
+}
+
+function ChangeMode1(){
+	alert("Global");
+	socket.emit('setMode',2);
+
+}
+
+function ChangeMode2(){
+	alert("Regional");
+	socket.emit('setMode',1);
+}
+
+function ChangeMode3(){
+	alert("Local");
+	socket.emit('setMode',0)
+}
+
+function groupingMode(chk) {
+	if(chk)
+	   	socket.emit('setMode', 3);
+	else
+		socket.emit('setMode', 4);
+}
+
+function DownVote() {
+    socket.emit('vote', 0);
+}
+
+function UpVote() {
+    socket.emit('vote', 1);
+}
+
+function downloadData() {
+    console.log('getting data...');
+    socket.emit('getData');
+}
+
+//$.unload(saveDataOnDb);
+
+function saveDataOnDb() {
+    console.log('userId is... ' + userData.id);
+    var userId = userData.id;
+    console.log('saving new session with id... ' + sessionId);
+    console.log('saving data...');
+    socket.emit('saveDataOnDb', userId, sessionId);
+}
+
+function getNumSessions() {
+  var numSessions = 0;
+  for (var key in userData) {
+    if (key.substring(0, 7) === "session") {
+      numSessions++;
+    }
+  }
+  return numSessions;
+}
+
+function TurnOnOffAgent() {
+    ison = !ison;
+    if (ison) {
+        console.log('turn agent on');
+        socket.emit('setAgentOn', true);
+		console.log(ison);
+    } else {
+        console.log('turn agent off');
+        socket.emit('setAgentOn', false);
+		console.log(ison);
+    }
+}
