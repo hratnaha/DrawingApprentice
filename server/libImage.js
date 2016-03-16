9e7dc2417e3c3a5bd164bec9e19c9fe555ee3a8b
Chipin Hsiao <chipin01@gmail.com>
2016-03-15 23:47:06 -0400

    include "canvas" drawing method for lib-image the utility for parsing svg to json

--- server/libImage.js
+++ server/libImage.js
@@ -5,10 +5,19 @@
 "use strict";
 
 var fs = require('fs'),
-    gm;
+    gm,
+    Canvas,
+    Image;
     
 try{
     gm = require('gm').subClass({imageMagick: true}); 
+}catch(err){
+    console.error(err);
+}
+
+try{
+    Canvas = require('canvas');
+    Image = Canvas.Image;
 }catch(err){
     console.error(err);
 }
@@ -34,50 +43,112 @@ function hexToRgb(hex) {
     } : null;
 }
 
+var isUsingGM = false;
+
 module.exports = {
     //Iinitialize the drawing context that has white background
     // and transparent filling in of the drawing lines
-    Initialize : function(width, height){
-        if(gm){
+    Initialize : function(width, height, useGM){
+        isUsingGM = useGM;
+        if(gm && useGM){
             var ctx = gm(width, height, "#ffffff")
                 .fill("transparent");
             return ctx;
+        }else if(Canvas){
+            var canvas = new Canvas(width, height)
+              , ctx = canvas.getContext('2d');
+            return ctx;
         }
+        return null;
     },
     InitializeFromFile : function(file){
-      if(gm){
-          var ctx = gm(file)
+        if(gm && isUsingGM){
+            var ctx = gm(file)
             .fill("transparent");
             return ctx;
-      }  
+        }else if(Canvas){
+            try{
+                var buff = fs.readFileSync(file);
+                var img = new Image;
+                img.src = buff;
+                var canvas = new Canvas(img.width, img.height)
+                  , ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                
+                return ctx;
+            }catch(err){
+                console.error(err);   
+            }
+        }
+        return null;
     },
     CreateBlankThumb: function(picName){
-        if(gm){
+        var imgpath = __dirname + '/session_pic/' + picName + '_thumb.png';
+        if(Canvas){
+            var canvas = new Canvas(80, 60)
+              , ctx = canvas.getContext('2d')
+              , out = fs.createWriteStream(imgpath)
+              , stream = canvas.pngStream();
+
+            stream.on('data', function(chunk){
+                out.write(chunk);
+            });
+
+            stream.on('end', function(){
+                console.log('saved png');
+            });
+        }
+        else if(gm){
             var ctx = gm(80, 60, "#ffffff");
-            ctx.write(__dirname + '/session_pic/' + picName + '_thumb.png', function (err) {
-                    if(err)
-                        console.log(err);
+            ctx.write(imgpath, function (err) {
+                if(err)
+                    console.log(err);
             });
         }
     },
     DrawLine : function(ctx, line, translate){
         translate = translate ? translate: {x : 0, y : 0};
         if(line.allPoints && line.allPoints.length > 0){
-            var lineColor;
-            if(line.color){
-                lineColor = rgbToHex(line.color.r, line.color.g, line.color.b);
-            }else
-                lineColor = rgbToHex(line.colorR, line.colorG, line.colorB);
-            //console.log(hexColor);
-            ctx.stroke(lineColor, line.lineWidth);
-            var pts = [];
-            for(var ptID in line.allPoints){
-                var pt = line.allPoints[ptID];
-                pts.push(pt.x - translate.x, pt.y - translate.y);
+            if(Canvas){
+                if(line.color)
+                    ctx.strokeStyle = 'rgba(' + line.color.r +',' + line.color.g + ', ' + line.color.b + ', 1)';
+                else
+                    ctx.strokeStyle = 'rgba(' + line.colorR +',' + line.colorG + ', ' + line.colorB + ', 1)';
+                ctx.lineWidth = line.lineWidth;
+                
+                function drawLine(line){
+                    if(line.allPoints && line.allPoints.length > 0){
+                        ctx.beginPath();
+                        ctx.moveTo(line.allPoints[0].x - translate.x, line.allPoints[0].y - translate.y);
+
+                        for(var ptID in line.allPoints){
+                            if(ptID > 0){
+                                var pt = line.allPoints[ptID];
+                                ctx.lineTo(pt.x - translate.x, pt.y - translate.y); 
+                            }
+                        }
+                        ctx.stroke();
+                    }
+                }
+                
+                drawLine(line);
+            }else if(gm){
+                var lineColor;
+                if(line.color){
+                    lineColor = rgbToHex(line.color.r, line.color.g, line.color.b);
+                }else
+                    lineColor = rgbToHex(line.colorR, line.colorG, line.colorB);
+                //console.log(hexColor);
+                ctx.stroke(lineColor, line.lineWidth);
+                var pts = [];
+                for(var ptID in line.allPoints){
+                    var pt = line.allPoints[ptID];
+                    pts.push(pt.x - translate.x, pt.y - translate.y);
+                }
+                //console.log("num of points: " + pts.length);
+                //if(pts.length < 500)
+                ctx.drawPolyline(pts);
             }
-            //console.log("num of points: " + pts.length);
-            //if(pts.length < 500)
-            ctx.drawPolyline(pts);
         }
     },
     ConvertDrawingToPng: function(canvasSize, picName, userLines, computerLines){
