e8433cbf4db4ba09145b8a01a8f01564c5d659ca
Chipin Hsiao <chipin01@gmail.com>
2016-04-29 19:15:00 -0400

    change settings and delete unnecessary files

--- server/configuration/googleConfig.js
+++ server/configuration/googleConfig.js
@@ -2,10 +2,10 @@
 module.exports = {
     "clientID" 		        : "88221843053-ti20vnrpur3dsle5f88d95j2ck3qfkgq.apps.googleusercontent.com",
     "clientSecret"	        : "rWLki6oLWqiY-8me8xq9DnWM",
-    "callbackURL"			: "/auth/google/callback",    
+    "callbackURL"			: "http://adam.cc.gatech.edu/DrawingApprentice/auth/google/callback",    
     "profileFields"         : ['emails', 'first_name', 'last_name', 'gender', 'age_range'],
     "use_database"			: "false",
-    "host"					: "localhost",
+    "host"					: "130.207.124.45",
     "username"				: "root",
     "password"				: "",
     "database"				: ""
